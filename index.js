d1fdf3cf1a688ab069f76caa7370a185c282d777
Stan Doe <example@example.com>
2022-02-01 16:46:05 +0200

    Add divide function

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ let b = 2;
 function sum(a, b) {
     return a+b;
 }
+function divide(a, b) {
+    return a/b;
+}
 
 console.log(`Sum ${a} + ${b} equal`, sum(a,b));
+console.log(`Divide ${a} / ${b} equal`, divide(a,b));
