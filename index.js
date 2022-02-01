ed7e51a9129d127024d13b5409d0ec92297e49cf
Stan Doe <example@example.com>
2022-02-01 16:44:04 +0200

    Add multiply function

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ let b = 2;
 function sum(a, b) {
     return a+b;
 }
+function multiply(a, b) {
+    return a*b;
+}
 
 console.log(`Sum ${a} + ${b} equal`, sum(a,b));
+console.log(`Multiply ${a} * ${b} equal`, multiply(a,b));
